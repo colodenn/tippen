061e06b531d9ac4be570b394b6c7e3fe91ae4c47
cornelius <cornelius.denninger@gmail.com>
2023-07-28 16:54:19 +0200

    Kinda working but sound should only play on keypress and not looping

--- src-tauri/src/keyboard_listener.rs
+++ src-tauri/src/keyboard_listener.rs
@@ -1,27 +1,123 @@
 // ./src-tauri/src/keyboard_listener.rs
-use rdev::{listen, Event, EventType};
+use rdev::{listen, Event, EventType, Key};
+use rodio::OutputStream;
+use std::io::BufReader;
+fn play_click_sound(file: &str) {
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // // Load a sound from a file, using a path relative to Cargo.toml
+    // let file = BufReader::new(File::open(file).unwrap());
+    // // Decode that sound file into a source
+    // let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    let file_cache = std::fs::File::open(file).unwrap();
+    let sound = stream_handle.play_once(BufReader::new(file_cache)).unwrap();
+    sound.set_volume(0.5);
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    std::thread::sleep(std::time::Duration::from_millis(150));
+}
 
 pub fn run_listener<F>(emit: F)
 where
     F: Fn(&str, &str) + 'static,
 {
-    println!("test");
-    if let Err(error) = listen(move |event| callback(event, &emit)) {
-        println!("Error: {:?}", error)
-    }
+    let mut released = true;
+    if let Err(_error) = listen(move |event| callback(event, &emit, &mut released)) {}
 }
 
-fn callback<F: Fn(&str, &str)>(event: Event, emit: &F) {
-    println!("My callback {:?}", event);
-
+fn callback<F: Fn(&str, &str)>(event: Event, emit: &F, released: &mut bool) {
+    let mut file = "sounds/key2.wav";
     match event.name {
         Some(string) => {
-            println!("Some");
-            
-            println!("Some: {}", string);
+            match event.event_type {
+                EventType::KeyPress(key) => {
+                    match key {
+                        Key::Space => {
+                            file = "sounds/space.wav";
+                            *released = false;
+                        }
+                        Key::Tab => {
+                            file = "sounds/tab.wav";
+                            *released = false;
+                        }
+                        Key::ShiftLeft => {
+                            file = "sounds/shift.wav";
+                            *released = false;
+                        }
+                        Key::ControlLeft => {
+                            file = "sounds/ctrl.wav";
+                            *released = false;
+                        }
+                        Key::Backspace => {
+                            file = "sounds/back.wav";
+                            *released = false;
+                        }
+                        Key::CapsLock => {
+                            file = "sounds/caps.wav";
+                            *released = false;
+                        }
+                        Key::Return => {
+                            file = "sounds/ent.wav";
+                            *released = false;
+                        }
+                        Key::KeyA => {
+                            file = "sounds/key1.wav";
+                            *released = false;
+                        }
+                        Key::KeyS => {
+                            file = "sounds/key2.wav";
+                            *released = false;
+                        }
+                        Key::KeyD => {
+                            file = "sounds/key3.wav";
+                            *released = false;
+                        }
+                        _ => {
+                            *released = false;
+                        }
+                    }
+                    std::thread::spawn(move || {
+                        play_click_sound(file);
+                    });
+                }
+                _ => {}
+            }
+
             emit("Some", &string);
         }
-        None => {
-        }
+        None => match event.event_type {
+            EventType::KeyPress(key) => {
+                if *released {
+                    match key {
+                        Key::ShiftLeft => {
+                            file = "sounds/shift.wav";
+                            *released = false;
+                        }
+                        Key::CapsLock => {
+                            file = "sounds/caps.wav";
+                            *released = false;
+                        }
+                        Key::ControlLeft => {
+                            file = "sounds/shift.wav";
+                            *released = false;
+                        }
+                        _ => {
+                            file = "sounds/key3.wav";
+                            *released = false;
+                        }
+                    }
+                    std::thread::spawn(move || {
+                        play_click_sound(file);
+                    });
+                }
+            }
+            EventType::KeyRelease(key) => match key {
+                _ => {
+                    *released = true;
+                }
+            },
+            _ => {}
+        },
     }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, NativeImage};
-use tauri::{CustomMenuItem, SystemTrayMenuItem, MenuEntry, SystemTraySubmenu};
+use tauri::{CustomMenuItem, SystemTrayMenuItem, SystemTraySubmenu};
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
 mod keyboard_listener;
 use std::thread;
 
@@ -12,42 +12,69 @@ struct Payload {
 }
 
 fn main() {
+    // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
+    let enable = CustomMenuItem::new("enable".to_string(), "Disable").accelerator("Cmd+E");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
+    let volume_0 = CustomMenuItem::new("0".to_string(), "0%");
 
-  // here `"quit".to_string()` defines the menu item id, and the second parameter is the menu item label.
-let enable = CustomMenuItem::new("enable".to_string(), "Enable").accelerator("Cmd+E");
-let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("Cmd+Q");
-let volume_slider = CustomMenuItem::new("volume".to_string(), "Volume");
+    let volume_25 = CustomMenuItem::new("25".to_string(), "25%");
+    let volume_50 = CustomMenuItem::new("50".to_string(), "50%");
 
-let volume_menu = SystemTrayMenu::new().add_item(volume_slider);
-let volume = SystemTraySubmenu::new("Volume", volume_menu);
+    let volume_75 = CustomMenuItem::new("75".to_string(), "75%");
+    let volume_100 = CustomMenuItem::new("100".to_string(), "100%").selected();
 
-let switches_menu = SystemTrayMenu::new();
-let switches = SystemTraySubmenu::new("Switches", switches_menu);
+    let thock_1 = CustomMenuItem::new("thock_1", "Thocky Keys").selected();
+    let thock_2 = CustomMenuItem::new("thock_2", "Cloudy Keys");
 
-let tray_menu = SystemTrayMenu::new()
-.add_item(enable)
-.add_native_item(SystemTrayMenuItem::Separator)
-.add_submenu(volume)
-.add_submenu(switches)
+    let volume_menu = SystemTrayMenu::new()
+        .add_item(volume_0)
+        .add_item(volume_25)
+        .add_item(volume_50)
+        .add_item(volume_75)
+        .add_item(volume_100);
+    let volume = SystemTraySubmenu::new("Volume", volume_menu);
 
-.add_native_item(SystemTrayMenuItem::Separator)
+    let switches_menu = SystemTrayMenu::new().add_item(thock_1).add_item(thock_2);
+    let switches = SystemTraySubmenu::new("Switches", switches_menu);
 
-.add_item(quit);
-  let system_tray = SystemTray::new()
-    .with_menu(tray_menu);
-  tauri::Builder::default()
-    .system_tray(system_tray)
-    .on_system_tray_event(|_app, event| match event {
-      SystemTrayEvent::MenuItemClick { id, .. } => {
-        match id.as_str() {
-          "quit" => {
-            std::process::exit(0);
-          }
-          _ => {}
-        }
-      }
-      _ => {}
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(enable)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_submenu(volume)
+        .add_submenu(switches)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+    tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_system_tray_event(|_app, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                _ => {}
+            },
+            _ => {}
+        })
+        .setup(move |app| {
+            let wv = app.get_window("main").unwrap();
+
+            thread::spawn(move || {
+                keyboard_listener::run_listener(move |s: &str, s1: &str| {
+                    if let Err(err) = wv.emit(
+                        "keypress",
+                        Payload {
+                            mode: String::from(s),
+                            message: String::from(s1),
+                        },
+                    ) {
+                        eprintln!("Error while emitting event: {:?}", err);
+                    }
+                })
+            });
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
